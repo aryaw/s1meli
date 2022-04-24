4e66c95d5e4a44d2dffd2b48b65e49531bf6dd4b
Arya Wiratama <aryawiratama99@gmail.com>
2022-04-25 00:31:28 +0800

    update

---
+++ packages/cms/pengadaan/src/Migrations/2022_03_28_112220_pengadaan_history.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PengadaanHistory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengadaan_history', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('pengadaam_id')->unsigned();
+            $table->integer('approve_wakasek')->default(0);
+            $table->integer('approve_kepsek')->default(0);
+            $table->date('tgl_approve_wakasek')->nullable();
+            $table->date('tgl_approve_kepsek')->nullable();
+            $table->timestamps();
+
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pengadaan_history');
+    }
+}
