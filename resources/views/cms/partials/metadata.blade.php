660d7f4dd4013be91c212023e8d54beed8487da0
Arya Wiratama <aryawiratama99@gmail.com>
2022-05-10 00:01:30 +0800

    update

--- resources/views/cms/partials/metadata.blade.php
+++ resources/views/cms/partials/metadata.blade.php
@@ -3,7 +3,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">  
-    <title> {{ (!empty($title) ? $title.' | ' : '') }} {{ setting('admin.title') }}</title>
+    <?php /* <title> {{ (!empty($title) ? $title.' | ' : '') }} {{ setting('admin.title') }}</title> */ ?>
+    <title> Sistem Informasi Manajemen Sarana Prasarana </title>
     <meta content="" name="description" />
     <meta content="" name="author" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
